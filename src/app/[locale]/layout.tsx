98472684aff7f163c14fcfef23d401413a3767dc
naochizuki <basugandhisasangka@student.uns.ac.id>
2024-07-30 17:40:12 +0700

    refactor: google analytic

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Quicksand } from 'next/font/google'
 import { NextIntlClientProvider } from 'next-intl'
-import Script from 'next/script'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { getMessages } from 'next-intl/server'
 import theme from '@/config/theme'
@@ -12,7 +11,7 @@ const quicksand = Quicksand({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Basugandhi Sasangka Murti - Portfolio',
-  description: 'Basugandhi Sasangka Murti - Portfolio',
+  description: 'Basugandhi Sasangka Murti - Portfolio - Naochizuki',
 }
 
 export default async function RootLayout({
@@ -33,12 +32,6 @@ export default async function RootLayout({
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
         <GoogleAnalytics gaId="G-V0K7ZYN96D" />
-        {/* <Script src={`https://www.googletagmanager.com/gtag/js?id=G-V0K7ZYN96D`} />
-        <Script id="google-analytics">
-          {`
-            window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);gtag('js', new Date());gtag('config', 'G-V0K7ZYN96D');
-          `}
-        </Script> */}
       </head>
       <body className={quicksand.className}>
         <NextIntlClientProvider messages={messages}>
