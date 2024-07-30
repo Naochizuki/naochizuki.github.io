8cc1287cbd7a193322ec1aed289c87a8bfdebe0e
naochizuki <basugandhisasangka@student.uns.ac.id>
2024-07-30 14:33:25 +0700

    feat: google analytic

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Quicksand } from 'next/font/google'
 import { NextIntlClientProvider } from 'next-intl'
+import Script from 'next/script'
 import { getMessages } from 'next-intl/server'
 import theme from '@/config/theme'
 import '@/app/globals.css'
@@ -31,6 +32,14 @@ export default async function RootLayout({
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);gtag('js', new Date());gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}');
+          `}
+        </Script>
       </head>
       <body className={quicksand.className}>
         <NextIntlClientProvider messages={messages}>
